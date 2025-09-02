c06f62cda2402376958ae94ad58c34ceb51259b6
Wes <wes.glen.66@icloud.com>
2025-09-02 14:57:48 -0500

    updated images

--- app/data/dummyData.js
+++ app/data/dummyData.js
@@ -100,42 +100,46 @@ export const dummyTeams = {
 
 export const dummyEvents = {
   boys: [
-    { id: 'e1', type: 'tournament', name: '1 Day Slam', time: 'April 25-26 2026', image: '/tournaments/1-day-slam.png',
+    { id: 'e1', type: 'tournament', name: '1 Day Slam', time: 'April 25-26 2026', image: '/tournaments-new/1-day-slam.jpeg',
       details: { ageGroup: '--', date: 'April 25-26 2026', location: '--', entryFee: '--', manager: { name: '--', phone: '--', email: '--' } },
       registrationUrl: 'https://google.com' // Replace with actual registration URL
     },
-    { id: 'e2', type: 'tournament', name: 'Autism Awareness Tournament', time: 'April 11-12, 2026', image: '/tournaments/autism-awareness.png',
+    { id: 'e2', type: 'tournament', name: 'Autism Awareness Tournament', time: 'April 11-12, 2026', image: '/tournaments-new/elevate-tournament.jpeg',
       details: { ageGroup: '--', date: 'April 11-12, 2026', location: '--', entryFee: '--', manager: { name: '--', phone: '--', email: '--' } },
       registrationUrl: 'https://uat.sssl.io/O0PLzO' // Replace with actual registration URL
     },
-    { id: 'e3', type: 'tournament', name: 'Dome Debut Classic', time: 'March 28-29, 2026', image: '/tournaments/dome-debut-classic.png',
+    { id: 'e3', type: 'tournament', name: 'Dome Debut Classic', time: 'March 28-29, 2026', image: '/tournaments-new/dome-debut-classic.jpeg',
       details: { ageGroup: '--', date: 'March 28-29, 2026', location: '--', entryFee: '--', manager: { name: '--', phone: '--', email: '--' } },
       registrationUrl: 'https://uat.sssl.io/4Q3PZR' // Replace with actual registration URL
     },
-    { id: 'e4', type: 'tournament', name: 'Top 100 Classic', time: 'August 8-9, 2026', image: '/tournaments/top-100-classic.png',
+    { id: 'e4', type: 'tournament', name: 'Top 100 Showcase', time: 'August 8-9, 2026', image: '/tournaments-new/top-100-showcase.jpeg',
       details: { ageGroup: '--', date: 'August 8-9, 2026', location: '--', entryFee: '--', manager: { name: '--', phone: '--', email: '--' } },
       registrationUrl: 'https://uat.sssl.io/46kvlO' // Replace with actual registration URL
     },
-    { id: 'e5', type: 'tournament', name: 'Nationals', time: 'July 26-27, 2026', image: '/tournaments/nationals.png',
-      details: { ageGroup: '--', date: 'July 26-27, 2026', location: '--', entryFee: '--', manager: { name: '--', phone: '--', email: '--' } },
+    { id: 'e5', type: 'tournament', name: 'Nationals', time: 'July 17-19, 2026', image: '/tournaments-new/nationals.jpg',
+      details: { ageGroup: '--', date: 'July 17-19, 2026', location: '--', entryFee: '--', manager: { name: '--', phone: '--', email: '--' } },
       registrationUrl: 'https://uat.sssl.io/Xw7yoX' // Replace with actual registration URL
     },
-    { id: 'e6', type: 'tournament', name: 'Queens Classic', time: 'May 9th, 2026', image: '/tournaments/queens-classic.png',
+    { id: 'e6', type: 'tournament', name: 'Queens Classic', time: 'May 9th, 2026', image: '/tournaments-new/queens-classic.jpeg',
       details: { ageGroup: '--', date: 'May 9th, 2026', location: '--', entryFee: '--', manager: { name: '--', phone: '--', email: '--' } },
       registrationUrl: 'https://google.com' // Replace with actual registration URL
     },
-    { id: 'e7', type: 'tournament', name: 'Summer Tipoff Classic', time: 'May 23rd-24th, 2026', image: '/tournaments/summer-tipoff-classic.png',
+    { id: 'e7', type: 'tournament', name: 'Summer Tipoff Classic', time: 'May 23rd-24th, 2026', image: '/tournaments-new/summer-tipoff-classic.jpg',
       details: { ageGroup: '--', date: 'May 23rd-24th, 2026', location: '--', entryFee: '--', manager: { name: '--', phone: '--', email: '--' } },
       registrationUrl: 'https://google.com' // Replace with actual registration URL
     },
-    { id: 'e8', type: 'tournament', name: 'Summer Slam Part 1', time: 'July 6th, 2026', image: '/tournaments/summer-slam-part-1.png',
-      details: { ageGroup: '--', date: 'July 6th, 2026', location: '--', entryFee: '--', manager: { name: '--', phone: '--', email: '--' } },
+    { id: 'e8', type: 'tournament', name: 'Summer Slam', time: 'July 6-7, 2026', image: '/tournaments-new/summer-slam.jpeg',
+      details: { ageGroup: '--', date: 'July 6-7, 2026', location: '--', entryFee: '--', manager: { name: '--', phone: '--', email: '--' } },
       registrationUrl: 'https://google.com' // Replace with actual registration URL
     },
-    { id: 'e9', type: 'tournament', name: 'Summer Slam Part 2', time: 'July 7th, 2026', image: '/tournaments/summer-slam-part-2.png',
-      details: { ageGroup: '--', date: 'July 7th, 2026', location: '--', entryFee: '--', manager: { name: '--', phone: '--', email: '--' } },
+    { id: 'e9', type: 'tournament', name: 'TeamCrew Evaluation', time: 'October 11th, 2026', image: '/tournaments-new/teamcrew-evaluation.jpg',
+      details: { ageGroup: '--', date: 'October 11th, 2026', location: '--', entryFee: '--', manager: { name: '--', phone: '--', email: '--' } },
       registrationUrl: 'https://google.com' // Replace with actual registration URL
     },
+    { id: 'e10', type: 'tournament', name: 'Avengers Tournament', time: 'May 3-4, 2026', image: '/tournaments-new/avengers.jpeg',
+      details: { ageGroup: '--', date: 'May 3-4, 2026', location: '--', entryFee: '--', manager: { name: '--', phone: '--', email: '--' } },
+      registrationUrl: 'https://google.com' // Replace with actual registration URL
+    }    
   ]
 };
 
